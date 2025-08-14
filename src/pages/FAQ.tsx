c58f45defddcf125a49ea18bf6b3e336fd9b6c4a
pirestairone6-sudo <pirestairone6@gmail.com>
2025-08-14 01:31:31 -0400

    Update FAQ.tsx

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React from 'react';
 import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
